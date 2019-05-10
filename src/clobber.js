46662a74026baaf814f4ec42b7ae064bf2c03d3a
Wesley Porter <wesp@qualtrics.com>
2019-05-10 14:59:45 -0600

    Add basic static assets

---
+++ src/clobber.js
@@ -0,0 +1,11 @@
+(() => {
+	function Game(canvas) {
+		const ctx = canvas.getContext("2d");
+		ctx.beginPath();
+		ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+		ctx.stroke();
+	}
+
+	const canvas = document.getElementsByClassName("game")[0];
+	Game(canvas)
+})();
