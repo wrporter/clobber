53bea76ef8437f082caa83a19de9e6c7b293fd98
Wesley Porter <wesp@qualtrics.com>
2019-05-24 09:39:33 -0600

    #13: Update test coverage reporting

--- .ci/coverage.sh
+++ .ci/coverage.sh
@@ -3,10 +3,6 @@
 set -e
 source .ci/config.sh
 
-docker run --rm --entrypoint="ls" ${TARGET_IMAGE}:${VERSION}
-
 container_id=$(docker create ${TARGET_IMAGE}:${VERSION})
-
 docker cp ${container_id}:/src/coverage $(pwd)/coverage
-
 docker rm -v ${container_id}
